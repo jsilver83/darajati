8096fe65f0992ea0923340c0110d183f8f2c7eaa
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-18 15:51:36 +0300

    added UserProfile to Admin

--- darajati/attendances_and_grades/admin.py
+++ darajati/attendances_and_grades/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(UserProfile)
 admin.site.register(Student)
 admin.site.register(Instructor)
 admin.site.register(Semester)
 admin.site.register(Department)
 admin.site.register(Course)
 admin.site.register(Section)
-admin.site.register(Enrolment)
+admin.site.register(Enrollment)
 admin.site.register(ScheduledPeriod)
 admin.site.register(AttendanceInstant)
 admin.site.register(Attendance)
