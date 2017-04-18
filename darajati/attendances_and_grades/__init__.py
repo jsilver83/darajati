643b6b23c96fc28a8eacbc89a75440195a2881e1
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-18 15:51:23 +0300

    Add signals to create profile for users

---
+++ darajati/attendances_and_grades/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'attendances_and_grades.apps.AttendancesAndGradesConfig'
--- darajati/attendances_and_grades/apps.py
+++ darajati/attendances_and_grades/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class AttendancesAndGradesConfig(AppConfig):
     name = 'attendances_and_grades'
+
+    def ready(self):
+        import attendances_and_grades.signals
