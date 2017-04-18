a7b31cdfefc32d63d57d4e6972a0e73e0cdb8b60
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-18 15:53:48 +0300

    Added 2 urls for the new views

--- darajati/attendances_and_grades/urls.py
+++ darajati/attendances_and_grades/urls.py
@@ -7,7 +7,7 @@ app_name = 'attendances_and_grades'
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^instructor/$', views.InstructorView.as_view(), name='instructor'),
-    # url(r'^section/(?P<section_id>[0-9]+)/$', views.SectionView.as_view(), name='section'),
-    # url(r'^section/(?P<section_id>[0-9]+)/students/$', views.SectionStudentView.as_view(), name='section_students'),
+    url(r'^section/(?P<section_id>[0-9]+)/$', views.SectionView.as_view(), name='section'),
+    url(r'^section/(?P<section_id>[0-9]+)/students/$', views.SectionStudentView.as_view(), name='section_students'),
     url(r'^unauthorized/$', TemplateView.as_view(template_name='unauthorized.html'), name='unauthorized'),
 ]
