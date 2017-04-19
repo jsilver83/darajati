e91330d8da2034758fce0c49b3f73fd097cd5797
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-19 10:46:31 +0300

    Renamed the urls of schedued_period and student

--- darajati/attendances_and_grades/urls.py
+++ darajati/attendances_and_grades/urls.py
@@ -7,7 +7,9 @@ app_name = 'attendances_and_grades'
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^instructor/$', views.InstructorView.as_view(), name='instructor'),
-    url(r'^section/(?P<section_id>[0-9]+)/$', views.SectionView.as_view(), name='section'),
-    url(r'^section/(?P<section_id>[0-9]+)/students/$', views.SectionStudentView.as_view(), name='section_students'),
+    url(r'^instructor/scheduled-period/(?P<period_id>[0-9]+)/$', views.ScheduledPeriodView.as_view(),
+        name='scheduled_period'),
+    url(r'^student/scheduled-period/(?P<period_id>[0-9]+)/$', views.SectionStudentView.as_view(),
+        name='student_section'),
     url(r'^unauthorized/$', TemplateView.as_view(template_name='unauthorized.html'), name='unauthorized'),
 ]
