078b98c48624aa3e6bb86abfaeeda34432ff49f1
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-23 15:39:35 +0300

    changed naming of the urls

--- darajati/enrollment/urls.py
+++ darajati/enrollment/urls.py
@@ -7,9 +7,9 @@ app_name = 'enrollment'
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^instructor/$', views.InstructorView.as_view(), name='instructor'),
-    url(r'^instructor/scheduled-period/(?P<period_id>[0-9]+)/$', views.ScheduledPeriodView.as_view(),
-        name='scheduled_period'),
+    url(r'^attendance/section/(?P<section_id>[0-9]+)/$', views.AttendanceView.as_view(),
+        name='section_attendance'),
     url(r'^student/section/(?P<section_id>[0-9]+)/$', views.SectionStudentView.as_view(),
-        name='student_section'),
+        name='section_students'),
     url(r'^unauthorized/$', TemplateView.as_view(template_name='unauthorized.html'), name='unauthorized'),
 ]
