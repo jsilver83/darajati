9510b351f92fed28040204f3caef65c4b5b840a6
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-25 11:05:22 +0300

    removed the attendace url from enrollment urls

--- darajati/enrollment/urls.py
+++ darajati/enrollment/urls.py
@@ -7,8 +7,6 @@ app_name = 'enrollment'
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^instructor/$', views.InstructorView.as_view(), name='instructor'),
-    url(r'^attendance/section/(?P<section_id>[0-9]+)/$', views.AttendanceView.as_view(),
-        name='section_attendance'),
     url(r'^student/section/(?P<section_id>[0-9]+)/$', views.SectionStudentView.as_view(),
         name='section_students'),
     url(r'^unauthorized/$', TemplateView.as_view(template_name='unauthorized.html'), name='unauthorized'),
