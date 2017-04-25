260ee5e40fbdc4587354e3a40efc1709d9dd7664
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-04-25 15:27:14 +0300

    added student_name, and apply the order for fields

--- darajati/attendance/forms.py
+++ darajati/attendance/forms.py
@@ -7,20 +7,28 @@ class AttendanceForm(forms.ModelForm):
     """
     Behavior: 
     - getting the tow fields from the model Attendance then add one more field to them
-    by calling the super class in the initial state and adding the field 'enrollment_id' as a hidden field
+    by calling the super class in the initial state and adding the field 'student_name' as disabled inputText field
     
-    - convert the enrollment field to a textInput then make it disable from change
+    - convert the enrollment field to a HiddenInput
+    
+    - add classes for css and re-order the list 
     """
+    ORDER = ('student_name', 'status', 'attendance_instant')
+
     def __init__(self, *args, **kwargs):
         super(AttendanceForm, self).__init__(*args, **kwargs)
-        self.fields['enrollment_id'] = forms.CharField(widget=forms.HiddenInput())
+        self.fields['student_name'] = forms.CharField(
+            widget=forms.TextInput(attrs={'disabled': 'disabled','class': 'form-control'}), required=False)
+        self.order_fields(self.ORDER)
 
     class Meta:
         model = Attendance
-        fields = ['enrollment', 'status']
+        fields = ['enrollment', 'status', 'attendance_instant']
         labels = {
-            'enrollment': _('Student Name:')
+            'student_name': _('Student Name:')
         }
         widgets = {
-            'enrollment': forms.TextInput(attrs={'disabled': 'disabled'}),
+            'enrollment': forms.HiddenInput(),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'attendance_instant': forms.Select(attrs={'class': 'form-control'})
         }
