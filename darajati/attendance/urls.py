8f495f9e474b2ffb7f13771ef9aa771fad3b3182
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-01 11:24:10 +0300

    url for a specific day within a section

--- darajati/attendance/urls.py
+++ darajati/attendance/urls.py
@@ -1,10 +1,12 @@
 from . import views
-from django.views.generic import TemplateView
 from django.conf.urls import url
 
 app_name = 'attendance'
 
 urlpatterns = [
     url(r'^section/(?P<section_id>[0-9]+)/$', views.AttendanceView.as_view(),
-        name='section_attendance'),
+        {'day': None}, name='section_attendance'),
+
+    url(r'^section/(?P<section_id>[0-9]+)/(?P<day>[A-Za-z]+)/$', views.AttendanceView.as_view(),
+        name='section_day_attendance'),
 ]
