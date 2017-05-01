900956a3b097235416bc7335e71bf9e2797592e5
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-01 11:20:37 +0300

    Fixed the name of AttendanceInstance in admin

--- darajati/attendance/admin.py
+++ darajati/attendance/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(ScheduledPeriod)
-admin.site.register(AttendanceInstant)
+admin.site.register(AttendanceInstance)
 admin.site.register(Attendance)
