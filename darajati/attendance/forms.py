72001a4dec61b8e7f32f865a4c23203f6a938f14
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-01 11:21:45 +0300

    Fixed duplicated entry for attendance

--- darajati/attendance/forms.py
+++ darajati/attendance/forms.py
@@ -3,6 +3,12 @@ from .models import Attendance
 from django.utils.translation import ugettext_lazy as _
 
 
+class Attendance1Form(forms.ModelForm):
+    class Meta:
+        model = Attendance
+        fields = '__all__'
+
+
 class AttendanceForm(forms.ModelForm):
     """
     Behavior: 
@@ -13,22 +19,33 @@ class AttendanceForm(forms.ModelForm):
     
     - add classes for css and re-order the list 
     """
-    ORDER = ('student_name', 'status', 'attendance_instant')
+    ORDER = ('student_name', 'status')
 
     def __init__(self, *args, **kwargs):
         super(AttendanceForm, self).__init__(*args, **kwargs)
         self.fields['student_name'] = forms.CharField(
-            widget=forms.TextInput(attrs={'disabled': 'disabled','class': 'form-control'}), required=False)
+            widget=forms.TextInput(attrs={'disabled': 'disabled', 'class': 'form-control'}), required=False)
+        self.fields['id'] = forms.IntegerField(widget=forms.HiddenInput())
+        self.fields['period'] = forms.CharField(
+            widget=forms.TextInput(attrs={'disabled': 'disabled', 'class': 'form-control'}), required=False)
         self.order_fields(self.ORDER)
 
     class Meta:
         model = Attendance
-        fields = ['enrollment', 'status', 'attendance_instant']
+        fields = ['enrollment', 'status', 'attendance_instance']
         labels = {
             'student_name': _('Student Name:')
         }
         widgets = {
             'enrollment': forms.HiddenInput(),
             'status': forms.Select(attrs={'class': 'form-control'}),
-            'attendance_instant': forms.Select(attrs={'class': 'form-control'})
+            'attendance_instance': forms.HiddenInput(),
         }
+
+    def save(self, commit=True):
+        self.instance.id = self.cleaned_data['id']
+        self.instance.status = self.cleaned_data['status']
+        if self.cleaned_data['status'] == 'pre':
+            print("ignore")
+        else:
+            super(AttendanceForm, self).save()
