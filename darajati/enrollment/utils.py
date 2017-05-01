066d3dd1e47e81a40bb97295fedd3f39262b44e6
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-01 11:27:29 +0300

    fixed get_offset_day to return 2 parameter

--- darajati/enrollment/utils.py
+++ darajati/enrollment/utils.py
@@ -2,6 +2,10 @@ from django.utils import timezone
 import datetime, calendar
 
 
+def get_offset_day(date, offset_days):
+    return date + datetime.timedelta(days=offset_days), day_string(date + datetime.timedelta(days=offset_days))
+
+
 def get_offset_time(time, offset_hours):
     return time + datetime.timedelta(hours=offset_hours)
 
@@ -16,3 +20,15 @@ def today():
 
 def current_day():
     return calendar.day_name[today().weekday()]
+
+
+def current_day_number():
+    today().weekday()
+
+
+def number_of_days(start_date, end_date):
+    return (start_date - end_date).days
+
+
+def day_string(date):
+    return calendar.day_name[date.weekday()]
