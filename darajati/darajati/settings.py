35043369040350404f578547bc66303dbadff538
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-14 16:07:38 +0300

    removed floppyforms

--- darajati/darajati/settings.py
+++ darajati/darajati/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'enrollment',
     'attendance',
     'grade',
-    'floppyforms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
