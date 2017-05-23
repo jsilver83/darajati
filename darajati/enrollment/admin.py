32e5c7368e8fced184b751d8081d067e1fc6f68b
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-23 15:51:16 +0300

    newline removal

--- darajati/enrollment/admin.py
+++ darajati/enrollment/admin.py
@@ -6,7 +6,6 @@ from django.utils.translation import ugettext_lazy as _
 admin.site.site_header = _('Darajati Admin')
 admin.site.index_title = _('Darajati Administration')
 
-
 admin.site.register(UserProfile)
 admin.site.register(Student)
 admin.site.register(Instructor)
