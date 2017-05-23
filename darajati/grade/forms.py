582d75227abee27abe0abdfeec05c2f66fd32f9e
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-23 15:56:47 +0300

    forms for grade

---
+++ darajati/grade/forms.py
@@ -0,0 +1,11 @@
+# from django import forms
+# from django.utils.safestring import mark_safe
+# from django.utils.translation import ugettext_lazy as _
+#
+# from .models import StudentGrade
+#
+#
+# class GradeForm(forms.ModelForm):
+#     class Meta:
+#         model = StudentGrade
+#         fields = ['enrollment', 'grade_break_down', 'grade_quantity', 'remarks']
