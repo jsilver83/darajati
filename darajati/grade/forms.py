dd9c059dfc9cab42c178864f90f5653f8e591879
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-05-29 11:57:29 +0300

    GradeForm, with forcuing the instructor to enter within the limit values

--- darajati/grade/forms.py
+++ darajati/grade/forms.py
@@ -1,11 +1,17 @@
-# from django import forms
-# from django.utils.safestring import mark_safe
-# from django.utils.translation import ugettext_lazy as _
-#
-# from .models import StudentGrade
-#
-#
-# class GradeForm(forms.ModelForm):
-#     class Meta:
-#         model = StudentGrade
-#         fields = ['enrollment', 'grade_break_down', 'grade_quantity', 'remarks']
+from django import forms
+
+from .models import StudentGrade, GradeBreakDown
+
+
+class GradesForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(GradesForm, self).__init__(*args, **kwargs)
+        max_value = GradeBreakDown.get_grade_break_down(self.initial['grade_break_down'])
+        max_value = max_value.weight
+        self.fields['grade_quantity'] = forms.DecimalField(
+            max_value=max_value,
+            min_value=0.0)
+
+    class Meta:
+        model = StudentGrade
+        fields = ['enrollment', 'grade_break_down', 'grade_quantity', 'remarks']
