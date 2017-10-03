78508eb85c4fdb0b5208235541a3c367a88b21e1
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-10-03 13:27:40 +0300

    banner_integration: forms: add 2 fields commit and detail_report

--- darajati/banner_integration/forms.py
+++ darajati/banner_integration/forms.py
@@ -5,7 +5,16 @@ from django.utils.translation import ugettext_lazy as _
 class CourseOfferingForm(forms.Form):
     def __init__(self, choices, *args, **kwargs):
         super(CourseOfferingForm, self).__init__(*args, **kwargs)
-        self.fields['course_offering'] = forms.ChoiceField(label=_('Course Offering'),
-                                                           choices=choices,
-                                                           required=True,
-                                                           widget=forms.Select(attrs={'class': 'thm-field'}))
+        self.fields['course_offering'] = forms.ChoiceField(
+            label=_('Course Offering'),
+            choices=choices,
+            required=True,
+            widget=forms.Select(attrs={'class': 'thm-field'}))
+        self.fields['commit_changes'] = forms.BooleanField(
+            label=_('Commit changes'),
+            required=False,
+        )
+        self.fields['detail_report'] = forms.BooleanField(
+            label=_('Detail report'),
+            required=False,
+        )
