5badee3385570fb166b90a4e262e52278c73bed2
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-10-03 13:34:50 +0300

    banner_integration: passing extra parameter to templates

--- darajati/banner_integration/views.py
+++ darajati/banner_integration/views.py
@@ -15,9 +15,11 @@ class PopulationRosterView(LoginRequiredMixin, FormView):
     form_class = CourseOfferingForm
     template_name = 'banner_integration/roster_populate.html'
     success_url = reverse_lazy('banner_integration:home')
+
     section_report = None
     student_report = None
     enrollment_report = None
+    inactive_sections_count = None
 
     def get_form_kwargs(self):
         """
@@ -32,10 +34,27 @@ class PopulationRosterView(LoginRequiredMixin, FormView):
         This will consist of the creation of the sections and assigning students to these section
           From there Faculty will be assigned to that section.
         """
-        self.section_report, self.student_report = initial_roster_creation(form.cleaned_data['course_offering'])
+        self.section_report, \
+        self.student_report, \
+        self.enrollment_report, \
+        self.inactive_sections_count = initial_roster_creation(
+            form.cleaned_data['course_offering'],
+            form.cleaned_data['commit_changes'])
+
         context = self.get_context_data(**kwargs)
 
-        context['section_report'] = self.section_report
-        context['student_report'] = self.student_report
+        context['report'] = False
+        context['comment'] = _("There are no changes")
+        if self.section_report or self.student_report \
+                or self.enrollment_report or self.inactive_sections_count:
+            context['report'] = True
+            context['section_report'] = self.section_report
+            context['student_report'] = self.student_report
+            context['enrollment_report'] = self.enrollment_report
+            context['inactive_sections_count'] = self.inactive_sections_count
+            context['detail_report'] = False
+
+            if form.cleaned_data['detail_report']:
+                context['detail_report'] = True
 
         return self.render_to_response(context)
