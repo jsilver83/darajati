350804ace5cf857771acce32cf932a1e4cbec138
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-10-11 10:37:38 +0300

    enrollment: implement more method to help with datetime. start end of the week

--- darajati/enrollment/utils.py
+++ darajati/enrollment/utils.py
@@ -2,6 +2,30 @@ from django.utils import timezone
 import datetime, calendar
 
 
+def get_previous_week(date):
+    return date + datetime.timedelta(days=-7)
+
+
+def get_next_week(date):
+    return date + datetime.timedelta(days=7)
+
+
+def get_start_end_dates_of_the_week(date):
+
+    start = date - datetime.timedelta(days=(date.weekday() + 1) % 7)
+    end = start + datetime.timedelta(days=4)
+    return start, end
+
+
+def get_dates_in_between(date):
+    dates = []
+    start, end = get_start_end_dates_of_the_week(date)
+    delta = end - start
+    for i in range(delta.days + 1):
+        dates.append(start + datetime.timedelta(days=i))
+    return dates
+
+
 def get_offset_day(date, offset_days):
     return date + datetime.timedelta(days=offset_days), day_string(date + datetime.timedelta(days=offset_days))
 
