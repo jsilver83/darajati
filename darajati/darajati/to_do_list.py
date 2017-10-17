5ba1648ad0a4f63888486591cb1eb65ccda8cc6b
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-10-17 16:44:56 +0300

    darajati: html files and migrations

---
+++ darajati/darajati/to_do_list.py
@@ -0,0 +1,35 @@
+# TODO List
+
+"""
+1. When changing status of attendance show the corresponding color:
+
+    Absent -> Red
+    Late -> Yellow
+    Excused -> Green-ish
+    
+2. Do not navigate from a page when someone have changed something without saving.
+
+    This will take effect for Attendances and Grades.
+    
+#DONE - 3. Flag for Don't calculate this fragment into the overall total of the student.
+
+#DONE - 4. Divide Boundary Range into 2 fields Upper Range aka + and Lower Range aka - .
+
+#DONE - 5. Add flag to disallow changes after submitting an attendance in course offering.
+
+6. Dynamically calculate section average while the instructor entering grades using JS.
+
+#DONE  - 7. Fix the days of the attendances.
+
+8. Grades Fragment Import.
+
+9. WebService 
+
+10. Absent & Late Deduction (Complex)
+ 
+#DONE - 11. Averages
+
+#DONE - 12. Add Serial Number for enrollments
+
+#DONE - 13. Order periods based on the start_time in attendance page.
+"""
