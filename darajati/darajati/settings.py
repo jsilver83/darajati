e11180217c70615a00972867da1a18387c8c751e
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2017-12-10 08:28:17 +0300

    removed debug toolbar package

--- darajati/darajati/settings.py
+++ darajati/darajati/settings.py
@@ -45,11 +45,11 @@ INSTALLED_APPS = [
     'simple_history',
     'django_celery_results',
     'impersonate',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
