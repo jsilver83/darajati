fb650b3eec9053e6ee55cce9b51924541a316b88
Mohammed Alnajdi <malnajdi@kfupm.edu.sa>
2018-07-22 09:44:53 +0300

    changed to the new sync implementation

--- darajati/banner_integration/utils.py
+++ darajati/banner_integration/utils.py
@@ -376,11 +376,20 @@ class Synchronization(object):
         self.section = None
         self.crns = []
         self.student = None
-        self.enrollment = None
+        self.none = None
         self.new_enrollments = []
         self.current_enrollments = []
         self.old_enrollments = []
         self.inactive_enrollment = []
+        self.all_sections = []
+        # instructor
+        self.instructor = None
+        self.faculty_period = None
+
+        # Periods
+        self.current_day = None
+        self.period = None
+        self.non_created_periods = []
 
     def get_roster_information(self):
         """
@@ -442,7 +451,6 @@ class Synchronization(object):
             self.create_or_activate_sections()
             self.create_or_get_student()
             self.assign_or_change_enrollment()
-            if index == 2: break
 
         if self.commit:
             self.commit_roaster_changes()
@@ -459,6 +467,7 @@ class Synchronization(object):
                 crn=self.result['crn'],
                 active=True
             )
+            self.all_sections.append(self.section)
             self.non_created_sections.append(self.section)
             self.crns.append(self.result['crn'])
 
@@ -472,6 +481,7 @@ class Synchronization(object):
             )
             if not self.section.active and self.section.crn not in self.crns:
                 self.section.active = True
+                self.all_sections.append(self.section)
                 self.changed_sections.append(self.section)
                 self.crns.append(self.section.crn)
 
@@ -517,7 +527,7 @@ class Synchronization(object):
                     old_enrollment.letter_grade = 'MOVED'
                     self.old_enrollments.append(old_enrollment)
 
-            if self.is_grade_has_a_letter():
+            if self.is_grade_has_a_letter() and self.enrollment.active:
                 self.enrollment.comment = 'Dropped with grade {}'.format(str(self.result['grade']).lower())
                 self.enrollment.updated_by = self.current_user
                 self.enrollment.active = False
@@ -529,21 +539,21 @@ class Synchronization(object):
                 student=self.student,
                 section=self.section
             )
-            self.enrollment.letter_grade = self.result['grade']
-            self.enrollment.active = True
 
-            if self.is_grade_has_a_letter():
+            self.enrollment.letter_grade = self.result['grade']
+            if self.is_grade_has_a_letter() and self.enrollment.active:
                 self.enrollment.active = False
                 self.enrollment.comment = 'Dropped with grade {}'.format(str(self.result['grade']).lower())
                 self.inactive_enrollment.append(self.enrollment)
             else:
+                self.enrollment.active = True
                 self.current_enrollments.append(self.enrollment)
 
     def is_grade_has_a_letter(self):
         """
         :return:
         """
-        current_letter_grade = str(self.result['grade']).lower()
+        current_letter_grade = str(self.enrollment.letter_grade).lower()
         letter_grades = ['w', 'wp', 'wf', 'ic', 'dn']
         return True if current_letter_grade in letter_grades else False
 
@@ -562,10 +572,116 @@ class Synchronization(object):
             for inactive_enrollment in self.inactive_enrollment:
                 inactive_enrollment.save()
 
-    def roaster_report(self):
-        print("Sections to be created: ", len(self.non_created_sections))
-        print("Sections to be changed: ", len(self.changed_sections))
-        print("New Enrollments: ", len(self.new_enrollments))
-        print("Current Enrollments: ", len(self.current_enrollments))
-        print("Old enrollments: ", len(self.old_enrollments))
-        print("Inactive enrollments: ", len(self.inactive_enrollment))
+    def faculty_initiation(self):
+        """
+        :return:
+        """
+        for self.section in self.all_sections:
+            faculties_periods = self.get_section_faculties(self.section)
+            for self.faculty_period in faculties_periods:
+                if not self.faculty_period['email'] and not self.faculty_period['user']:
+                    continue
+                self.create_or_get_instructor()
+                self.create_or_get_periods()
+
+        if self.commit:
+            self.commit_faculties_periods()
+
+    def create_or_get_instructor(self):
+        """
+        :return:
+        """
+        user, created = User.objects.get_or_create(username=self.faculty_period['user'])
+        self.instructor, created = Instructor.objects.get_or_create(
+            user=user,
+            university_id=self.faculty_period['fac_id']
+        )
+        self.instructor.government_id = self.faculty_period['fac_id']
+        self.instructor.english_name = self.faculty_period['name']
+        self.instructor.arabic_name = self.faculty_period['name']
+        self.instructor.personal_email = self.faculty_period['email']
+        self.instructor.active = True
+        self.instructor.save()
+
+    def create_or_get_periods(self):
+        """
+        :return:
+        """
+        for day in map(str, self.faculty_period['class_days']):
+            self.current_day = self.get_period_current_day(day)
+            start_time = self.get_period_start_time()
+            end_time = self.get_period_end_time()
+            if not ScheduledPeriod.is_period_exists(self.section, self.instructor, self.current_day, start_time, end_time):
+                self.period = ScheduledPeriod(
+                    section=self.section,
+                    instructor_assigned=self.instructor,
+                    day=self.current_day,
+                    title=self.faculty_period['activity'],
+                    start_time=start_time,
+                    end_time=end_time,
+                    location=self.faculty_period['bldg'] + ' ' + self.faculty_period['room'],
+                )
+                self.non_created_periods.append(self.period)
+
+    def get_period_current_day(self, day):
+        if day == 'U':
+            return ScheduledPeriod.Days.SUNDAY
+        if day == 'M':
+            return ScheduledPeriod.Days.MONDAY
+        if day == 'T':
+            return ScheduledPeriod.Days.TUESDAY
+        if day == 'W':
+            return ScheduledPeriod.Days.WEDNESDAY
+        if day == 'R':
+            return ScheduledPeriod.Days.THURSDAY
+
+    def get_period_start_time(self):
+        start_time = list(map(str, self.faculty_period['start_time']))
+        start_time = start_time[0] + start_time[1] + ':' + start_time[2] + start_time[3]
+        return start_time
+
+    def get_period_end_time(self):
+        end_time = list(map(str, self.faculty_period['end_time']))
+        end_time = end_time[0] + end_time[1] + ':' + end_time[2] + end_time[3]
+        return end_time
+
+    def commit_faculties_periods(self):
+        with transaction.atomic():
+            for new_period in self.non_created_periods:
+                new_period.save()
+
+    def sections_report(self):
+        report = []
+        for new_section in self.non_created_sections:
+            report.append(
+                {'section': new_section, 'code': 'CREATE', 'message': 'New section to be created.'}
+            )
+        for changed_section in self.changed_sections:
+            report.append(
+                {'section': changed_section, 'code': 'CHANGED', 'message': 'section has been changed.'}
+            )
+        return report
+
+    def enrollments_report(self):
+        report = []
+        for new_enrollment in self.new_enrollments:
+            report.append(
+                {'enrollment': new_enrollment, 'code': 'CREATE', 'message': 'New enrollment to be created.'}
+            )
+        for old_enrollment in self.old_enrollments:
+            report.append(
+                {'enrollment': old_enrollment, 'code': 'CHANGED', 'message': 'Enrollment has been changed.'}
+            )
+        for inactive_enrollment in self.inactive_enrollment:
+            report.append(
+                {'enrollment': inactive_enrollment, 'code': 'INACTIVE', 'message': 'Enrollment will be set to inactive.'}
+            )
+        return report
+
+    def faculties_periods_report(self):
+        report = []
+        for non_created_period in self.non_created_periods:
+            report.append(
+                {'period': non_created_period, 'code': 'CREATE', 'message': 'Period to be created.'}
+            )
+        return report
