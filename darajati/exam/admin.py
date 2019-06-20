452ac11b66f285b5124bb78ec0f3266d14bbb6ef
Abdullah Joud <joud@kfupm.edu.sa>
2019-06-20 11:49:11 +0300

    admin enhancements on exams apps. used autocomplete wherever applicable.

--- darajati/exam/admin.py
+++ darajati/exam/admin.py
@@ -8,22 +8,38 @@ from .models import *
 
 
 class MarkerAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ('instructor', 'exam_room', )
     history_list_display = ['instructor', 'exam_room', 'order', 'generosity_factor', 'is_a_monitor']
-    list_filter = ('exam_room__exam_shift__settings', 'instructor', 'exam_room', 'order', 'is_a_monitor', )
+    list_filter = ('exam_room__exam_shift__settings__fragment__course_offering__semester',
+                   'exam_room__exam_shift__settings__fragment__course_offering__course', 'order', 'is_a_monitor', )
     list_display = ('instructor', 'exam_room', 'order', 'generosity_factor', 'is_a_monitor', 'updated_by', )
+    search_fields = ('exam_room__exam_shift__settings__fragment__course_offering__semester',
+                     'exam_room__exam_shift__settings__fragment__course_offering__course', 'instructor__english_name',
+                     'instructor__arabic_name', 'exam_room__room')
+    readonly_fields = ('updated_by', )
 
 
 class StudentPlacementAdmin(admin.ModelAdmin):
-    list_filter = ('exam_room', 'is_present', )
+    autocomplete_fields = ('enrollment', 'exam_room', )
+    list_filter = ('exam_room__exam_shift__settings__fragment__course_offering__semester',
+                   'exam_room__exam_shift__settings__fragment__course_offering__course', 'exam_room__room',
+                   'is_present', )
     list_display = ('enrollment', 'exam_room', 'is_present', 'shuffled_by', 'shuffled_on', )
+    search_fields = ('enrollment__student__university_id', 'enrollment__student__english_name',
+                     'enrollment__student__arabic_name', )
+    readonly_fields = ('shuffled_by', 'shuffled_on', )
 
 
 class StudentMarkAdmin(SimpleHistoryAdmin):
+    autocomplete_fields = ('student_placement', 'marker', )
     history_list_display = ['student_placement', 'marker', 'mark']
-    list_filter = ('marker__exam_room__exam_shift__settings', 'marker__exam_room', 'marker__instructor', )
+    list_filter = ('marker__exam_room__exam_shift__settings__fragment__course_offering__semester',
+                   'marker__exam_room__exam_shift__settings__fragment__course_offering__course',
+                   'marker__exam_room__room', 'marker__order', )
     list_display = ('student_placement', 'marker', 'mark', 'get_generosity_factor', 'show_weighted_mark',
                     'updated_on', 'updated_by')
-    search_fields = ('student_placement__enrollment__student__user__username', )
+    search_fields = ('student_placement__enrollment__student__university_id', )
+    readonly_fields = ('updated_by', )
 
     def get_queryset(self, request):
         return super(StudentMarkAdmin,self).get_queryset(request).select_related('marker')
@@ -53,16 +69,30 @@ class StudentMarkAdmin(SimpleHistoryAdmin):
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'location', 'capacity', 'updated_by', 'updated_on', )
     list_editable = ('location', 'capacity', )
+    search_fields = ('name', 'location', )
+    readonly_fields = ('updated_by', )
+
+
+class ExamShiftAdmin(admin.ModelAdmin):
+    list_display = ('settings', 'start_date', 'end_date', )
+    list_editable = ('start_date', 'end_date', )
+    list_filter = ('settings__fragment__course_offering__semester', 'settings__fragment__course_offering__course', )
+    search_fields = ('settings__fragment__course_offering__semester', 'settings__fragment__course_offering__course',
+                     'start_date', 'end_date', )
 
 
 class ExamRoomAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('exam_shift', 'room', )
     list_display = ('exam_shift', 'room', 'capacity', 'remaining_seats', )
-    list_filter = ('exam_shift__settings__fragment__course_offering', 'exam_shift__settings', 'exam_shift', 'room', )
+    list_filter = ('exam_shift__settings__fragment__course_offering__semester',
+                   'exam_shift__settings__fragment__course_offering__course', 'exam_shift__settings__fragment',
+                   'exam_shift', 'room', )
     list_editable = ('capacity', )
+    search_fields = ('room', )
 
 
 admin.site.register(ExamSettings)
-admin.site.register(ExamShift)
+admin.site.register(ExamShift, ExamShiftAdmin)
 admin.site.register(StudentPlacement, StudentPlacementAdmin)
 admin.site.register(Marker, MarkerAdmin)
 admin.site.register(StudentMark, StudentMarkAdmin)
